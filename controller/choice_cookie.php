868a3ae31d150b63ba7b0772c5f7d7f7046c0a02
giusmili <giusmili67@gmail.com>
2025-01-15 19:03:02 +0100

    php controller

---
+++ controller/choice_cookie.php
@@ -0,0 +1,8 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Sauvegarde des paramètres personnalisés
+        setcookie('analytics_cookies', $_POST['analytics'] ?? 'off', time() + (365 * 24 * 60 * 60), "/");
+        setcookie('ads_cookies', $_POST['ads'] ?? 'off', time() + (365 * 24 * 60 * 60), "/");
+        header("Location: index.php");
+        exit();
+    }
--- customize_cookies.php
+++ customize_cookies.php
@@ -1,11 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Sauvegarde des paramètres personnalisés
-    setcookie('analytics_cookies', $_POST['analytics'] ?? 'off', time() + (365 * 24 * 60 * 60), "/");
-    setcookie('ads_cookies', $_POST['ads'] ?? 'off', time() + (365 * 24 * 60 * 60), "/");
-    header("Location: index.php");
-    exit();
-}
+include_once __DIR__ .'/route/choice_cookie.php';
 
 include_once __DIR__ .'/route/header.inc.php'
 ?>
