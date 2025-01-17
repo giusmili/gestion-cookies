e2ac4207fa3b13c888262e8fbeb364c23f1e70bb
giusmili <giusmili67@gmail.com>
2025-01-17 12:00:59 +0100

    class user

--- controller/controllerCookie.php
+++ controller/controllerCookie.php
@@ -1,15 +1,27 @@
 <?php
     # Vérification si un choix a été fait
-    if (isset($_POST['cookie_choice'])) {
-        $cookie_choice = $_POST['cookie_choice'];
-        setcookie('Portfolio', $cookie_choice, time() + (365 * 24 * 60 * 60), "/"); # Cookie valide pendant 1 an
-        if ($cookie_choice === 'customize') {
-            header("Location: customize_cookies.php");
+class User{
+
+    public function user(){
+        if (isset($_POST['cookie_choice'])) {
+            $cookie_choice = $_POST['cookie_choice'];
+            setcookie('Portfolio', $cookie_choice, time() + (365 * 24 * 60 * 60), "/"); # Cookie valide pendant 1 an
+            if ($cookie_choice === 'customize') {
+                header("Location: customize_cookies.php");
+                exit();
+            }
+            header("Location: index.php");
             exit();
         }
-        header("Location: index.php");
-        exit();
+    
+        
+     
     }
 
-    # Vérifie si un choix existe déjà
-    $user_cookie_choice = $_COOKIE['Portfolio'] ?? null;
+}
+# Vérifie si un choix existe déjà
+$user_cookie_choice = $_COOKIE['Portfolio'] ?? null;
+$cookie = new User();
+$cookie->user();
+
+   
