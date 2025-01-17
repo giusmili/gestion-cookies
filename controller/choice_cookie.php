c25d571136732dfc0a1c128c93220149289cdd28
giusmili <giusmili67@gmail.com>
2025-01-17 11:53:25 +0100

    php class coockie

--- controller/choice_cookie.php
+++ controller/choice_cookie.php
@@ -1,9 +1,17 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Sauvegarde des paramètres personnalisés
+class Cookie{
 
-        setcookie('analytics_cookies', $_POST['analytics'] ?? 'off', time() + (365 * 24 * 60 * 60), "/");
-        setcookie('ads_cookies', $_POST['ads'] ?? 'off', time() + (365 * 24 * 60 * 60), "/");
-        header("Location: index.php");
-        exit();
+    static function cookie(){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Sauvegarde des paramètres personnalisés
+    
+            setcookie('analytics_cookies', $_POST['analytics'] ?? 'off', time() + (365 * 24 * 60 * 60), "/");
+            setcookie('ads_cookies', $_POST['ads'] ?? 'off', time() + (365 * 24 * 60 * 60), "/");
+            header("Location: index.php");
+            exit();
+        }
     }
+
+}
+
+Cookie::cookie();
