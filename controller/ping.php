a2c054bb123109292d06cf7ec3e816f89dc4e394
giusmili <giusmili67@gmail.com>
2025-01-25 17:58:21 +0100

    class Ping

--- controller/ping.php
+++ controller/ping.php
@@ -1,17 +1,22 @@
 <?php
 
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        $content = file_get_contents(filename: 'php://input');
-
-        if($content === 'PING'){
-            $source = $_SERVER['HTTP_PING_FROM'];
-            $dest = $_SERVER['HTTP_PING_TO'];
-            $send_at = date(format: 'd/m/Y H:i:s');
-
-            file_put_contents(filename: 'ping.log', data: 
-            "$send_at - $content - De : $source - Vers $dest\n", flags: FILE_APPEND);
+    class Ping{
+        static function ping(){
+            if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                $content = file_get_contents(filename: 'php://input');
+        
+                if($content === 'PING'){
+                    $source = $_SERVER['HTTP_PING_FROM'];
+                    $dest = $_SERVER['HTTP_PING_TO'];
+                    $send_at = date(format: 'd/m/Y H:i:s');
+        
+                    file_put_contents(filename: 'ping.log', data: 
+                    "$send_at - $content - De : $source - Vers $dest\n", flags: FILE_APPEND);
+                }
+            }
         }
     }
+    Ping::ping();
 
 /* 
 
